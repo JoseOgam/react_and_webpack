435cad2f18cc29cde18b546bc6d8456b95d0d23d
JoseOgam <jotienoochieng@gmail.com>
2022-04-23 06:52:20 +0300

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+    filename: 'bundle.js'
+    },
+  
+  module: {
+    rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    }
+    ]
+  },
+};
